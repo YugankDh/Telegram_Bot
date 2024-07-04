ff109f27edd873a2294b30e0bb0b37ea0f729bc7
YugankDh <147908064+YugankDh@users.noreply.github.com>
2024-07-04 12:54:12 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,79 +1,69 @@
 const TelegramBot = require('node-telegram-bot-api');
-const quote_apiurl = "https://zenquotes.io/api/random"
-const keep_alive = require('./keep_alive.js")
-const fact_apiurl = "https://uselessfacts.jsph.pl/api/v2/facts/random"
-const meme_url = "https://meme-api.com/gimme"
-require('dotenv').config()
+const quote_apiurl = "https://zenquotes.io/api/random";
+const keep_alive = require('./keep_alive.js');
+const fact_apiurl = "https://uselessfacts.jsph.pl/api/v2/facts/random";
+const meme_url = "https://meme-api.com/gimme";
+require('dotenv').config();
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
 async function getFact() {
     try {
-        const factResponse = await fetch(fact_apiurl)
-        let factData = await factResponse.json()
-        return factData.text
+        const factResponse = await fetch(fact_apiurl);
+        let factData = await factResponse.json();
+        return factData.text;
     } catch (error) {
-        return "error occured"
+        return "error occurred";
     }
-
 }
 
 async function getQuote() {
     try {
-        const response = await fetch(quote_apiurl)
-        let data = await response.json()
-        return data[0].q
+        const response = await fetch(quote_apiurl);
+        let data = await response.json();
+        return data[0].q;
     } catch (error) {
-        return "error occured"
+        return "error occurred";
     }
-
 }
 
 async function memeTitle() {
     try {
-        const meme_titleresponse = await fetch(meme_url)
-        let data = await meme_titleresponse.json()
-        // console.log(data.title);
-        return data.title
+        const meme_titleresponse = await fetch(meme_url);
+        let data = await meme_titleresponse.json();
+        return data.title;
     } catch (error) {
-        return "error occured"
+        return "error occurred";
     }
-
-
 }
 
 async function memeImg() {
     try {
-        const memeresponse = await fetch(meme_url)
-        let data = await memeresponse.json()
-        return data.url
+        const memeresponse = await fetch(meme_url);
+        let data = await memeresponse.json();
+        return data.url;
     } catch (error) {
-        return "error occured"
+        return "error occurred";
     }
-
-
 }
-
 
 // Listen for any kind of message.
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
 
-    if (msg.text.toLocaleLowerCase() == "$quote") {
-        const quote = await getQuote()
-        bot.sendMessage(chatId, quote)
+    if (msg.text.toLocaleLowerCase() === "$quote") {
+        const quote = await getQuote();
+        bot.sendMessage(chatId, quote);
     }
 
-    if (msg.text.toLocaleLowerCase() == "$fact") {
-        const fact = await getFact()
-        bot.sendMessage(chatId, fact)
-    }
-    if (msg.text.toLocaleLowerCase() == "$meme") {
-        const meme = await memeImg()
-        // console.log(meme);
-        const memetitle = await memeTitle()
-        // console.log(memetitle);
-        bot.sendMessage(chatId, memetitle)
-        bot.sendPhoto(chatId, meme)
+    if (msg.text.toLocaleLowerCase() === "$fact") {
+        const fact = await getFact();
+        bot.sendMessage(chatId, fact);
     }
 
+    if (msg.text.toLocaleLowerCase() === "$meme") {
+        const meme = await memeImg();
+        const memetitle = await memeTitle();
+        bot.sendMessage(chatId, memetitle);
+        bot.sendPhoto(chatId, meme);
+    }
 });
